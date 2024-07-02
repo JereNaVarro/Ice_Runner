86526602cb42fa35e7cb9b28896958437ea87bc6
JereNaVarro <164099389+JereNaVarro@users.noreply.github.com>
2024-07-02 15:30:27 -0300

    cambios

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import Main from "./scenes/Main.js";
 
-// Create a new Phaser config object
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -24,11 +23,7 @@ const config = {
       debug: true,
     },
   },
-  // List of scenes to load
-  // Only the first scene will be shown
-  // Remember to import the scene before adding it to the list
   scene: [Main],
 };
 
-// Create a new Phaser game instance
 window.game = new Phaser.Game(config);
