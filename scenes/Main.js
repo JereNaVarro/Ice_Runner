13bc6c6236159cd4d45a9f5e61a33c6a696c0a2b
JereNaVarro <164099389+JereNaVarro@users.noreply.github.com>
2024-07-03 00:01:38 -0300

    agregados

--- scenes/Main.js
+++ scenes/Main.js
@@ -4,12 +4,14 @@ export default class Main extends Phaser.Scene {
   }
 
   init() {
+    this.playerLife = 1;
   }
   
   preload() {
     this.load.image("sky", "../public/assets/sky.webp");
     this.load.image("platform", "../public/assets/platform.png");
     this.load.image("gameover", "../public/assets/gameover.png");
+    this.load.image("ground", "../public/assets/ground.png");
     this.load.spritesheet("dude", "../public/assets/dude.png",{
     frameWidth: 32,
     frameHeight: 48,
@@ -19,8 +21,10 @@ export default class Main extends Phaser.Scene {
   create() {
     //MAPA
     this.add.image(400, 300, "sky").setScale(2);
+    this.ground = this.physics.add.image(400, 568, "ground").setImmovable().setScale(2);
+    this.ground.body.allowGravity = false;
     this.plataformas = this.physics.add.group();
-    const platform = this.plataformas.create(400, 568, "platform").setScale(2);
+    const platform = this.plataformas.create(400, 800, "platform").setScale(2);
     platform.setImmovable(true);
     platform.body.allowGravity = false;
     this.anims.create({
@@ -36,24 +40,43 @@ export default class Main extends Phaser.Scene {
     this.player.setBounce(0, 0.2);
     this.player.setCollideWorldBounds(true);
     this.physics.add.collider(this.player, this.plataformas);
-    //this.player.body.setGravityY(300)
+    this.physics.add.collider(this.player, this.ground);
+    this.player.body.setGravityY(800);
     this.player.anims.play("right", true);
     this.player.setCollideWorldBounds(true);
+    //plataformas
+    this.time.addEvent({
+      delay: 2000,
+      callback: this.createPlatform,
+      callbackScope: this,
+      loop: true,
+    });
+
+    this.physics.add.collider(this.player, this.plataformas, this.platformImpact, null, this);
   }
+
     
   update() {
       if (this.cursor.up.isDown && this.player.body.touching.down) {
-        this.player.setVelocityY(-230);
+        this.player.setVelocityY(-600);
       }
   }
 
+  createPlatform() {
+    const x = 800;
+    const y = 520;
+    const width = Phaser.Math.FloatBetween(0.2, 0.4);
+    const platform = this.plataformas.create(x, y, "platform");
+    platform.setScale(width, 1).refreshBody();
+    platform.setVelocityX(-200);
+    platform.setImmovable(true);
+    platform.body.allowGravity = false;
 
-
-
-
-
-
-
-
-
+  }   
+  platformImpact() {
+    this.playerLife = 0
+    console.log(this.playerLife); 
+  }
+    
+  
 }
